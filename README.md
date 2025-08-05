3829fb7292960cbe1c63404dbb67e806f1a32ca0
EricZzz <biggogten@gmail.com>
2025-08-05 19:55:23 +0800

    feat: initialize led-pwm project with basic functionality
    - Add Cargo.toml for project configuration and dependencies
    - Create build.rs for custom linker script handling
    - Define rust-toolchain.toml for toolchain specifications
    - Implement main.rs with LED PWM control and breathing effect
    - Add lib.rs to support no_std environment

---
+++ README.md
@@ -0,0 +1,15 @@
+Rust 嵌入式开发
+
+这是一个 Rust 嵌入式开发的示例项目，旨在展示如何使用 Rust 进行嵌入式系统编程。
+
+## 工具
+- [Rust](https://www.rust-lang.org/): Rust 编程语言。
+
+## 参考
+[《ESP32 团队写的 Rust SDK 开发参考文档》](https://esp32.implrust.com/index.html)
+[《The Rust on ESP Book 简体中文版》](https://narukara.github.io/rust-on-esp-book-zh-cn/introduction.html)
+
+使用芯片：ESP32-C3，[ESP32C3-CORE 开发板](https://wiki.luatos.com/chips/esp32c3/board.html)
+
+## 项目示例
+- `led-pwm`：使用 PWM 控制 LED 呼吸灯效果。
---
+++ led-pwm/src/bin/main.rs
@@ -0,0 +1,61 @@
+#![no_std]
+#![no_main]
+
+use esp_hal::clock::CpuClock;
+use esp_hal::gpio::{Level, Output, OutputConfig};
+use esp_hal::ledc::channel::ChannelIFace;
+use esp_hal::ledc::timer::TimerIFace;
+use esp_hal::ledc::{channel, timer, LSGlobalClkSource, Ledc, LowSpeed};
+use esp_hal::main;
+use esp_hal::time::{Duration, Instant, Rate};
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[main]
+fn main() -> ! {
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    let led = peripherals.GPIO12;
+    let mut ledc = Ledc::new(peripherals.LEDC);
+    ledc.set_global_slow_clock(LSGlobalClkSource::APBClk);
+    let mut lstimer0 = ledc.timer::<LowSpeed>(timer::Number::Timer0);
+    lstimer0
+        .configure(timer::config::Config {
+            duty: timer::config::Duty::Duty5Bit,
+            clock_source: timer::LSClockSource::APBClk,
+            frequency: Rate::from_khz(24),
+        })
+        .unwrap();
+
+    let mut channel0 = ledc.channel(channel::Number::Channel0, led);
+    channel0
+        .configure(channel::config::Config {
+            timer: &lstimer0,
+            duty_pct: 10,
+            pin_config: channel::config::PinConfig::PushPull,
+        })
+        .unwrap();
+
+    let mut led2 = Output::new(peripherals.GPIO13, Level::High, OutputConfig::default());
+    loop {
+        // led 呼吸灯效果
+        channel0.start_duty_fade(0, 100, 1000).unwrap();
+        while channel0.is_duty_fade_running() {}
+        channel0.start_duty_fade(100, 0, 1000).unwrap();
+        while channel0.is_duty_fade_running() {}
+        //led2 交替闪烁
+        // blocking_delay(Duration::from_millis(1000));
+        led2.toggle();
+    }
+}
+
+fn blocking_delay(duration: Duration) {
+    let delay_start = Instant::now();
+    while delay_start.elapsed() < duration {}
+}
